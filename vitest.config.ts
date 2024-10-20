83a54a7c31b0334d5a20a1b84b3d37af3b7e0d30
SamuelLorrain <samuel.lorrain@gmail.com>
2024-10-20 13:24:36 +0200

    config: allow public files in project and tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -7,5 +8,12 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*']
     },
+    globals: true,
+    environment: 'jsdom',
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  }
 })
