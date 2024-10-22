79bafcea355c7cbe260cefb9f4052eabc4376bb2
SamuelLorrain <samuel.lorrain@gmail.com>
2024-10-22 07:29:11 +0200

    ui(refactor): creating PanningListener abstraction on canvas panning functionality

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import blue from "@/assets/tiles/blue.png";
 import white from "@/assets/tiles/white.png";
 import whiteSelection from "@/assets/tiles/white-selection.png";
 import selection from "@/assets/tiles/selection.png";
+import CanvasPanningListener from "./ui/infra/CanvasPanningListener";
 
 const TILE_SIZE = new Vec2D(32, 16);
 let origin = new Vec2D();
@@ -51,55 +52,13 @@ window.addEventListener('load', () => {
   const cursor = Mouse.getInstance();
   const ctx = context2dProvider.ctx;
   const changeSizeObserver = new CanvasChangeSizeObserver();
+  const panningListener = new CanvasPanningListener(context2dProvider.canvas);
   changeSizeObserver.observe(context2dProvider.canvas);
 
   const tiles: ImageData[] = [];
   tiles.push(new ImageData(green));
 
   const grid = new Int32Array(10*10);
-
-  let dragging = false;
-  let drag = new Vec2D(0,0);
-  let dragstart = new Vec2D(0,0);
-  let currentDragging = new Vec2D(0,0);
-
-  const canvas = context2dProvider.canvas;
-
-  canvas.addEventListener('mousedown', (e: MouseEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dragging = true;
-    currentDragging = new Vec2D(drag);
-    dragstart.set(
-      e.clientX * window.devicePixelRatio,
-      e.clientY * window.devicePixelRatio
-    )
-  });
-
-  canvas.addEventListener('mouseup', (e: MouseEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dragging = false;
-  });
-
-  canvas.addEventListener('mousemove', (e: MouseEvent) => {
-    if (!dragging) {
-      return;
-    }
-    const currentPosition = new Vec2D(
-      e.clientX * window.devicePixelRatio,
-      e.clientY * window.devicePixelRatio
-    )
-    if (dragstart.almostEq(currentPosition, 2)) {
-      return;
-    }
-
-    drag.set(new Vec2D(
-      e.clientX * window.devicePixelRatio - dragstart.x + currentDragging.x,
-      e.clientY * window.devicePixelRatio - dragstart.y + currentDragging.y
-    ));
-
-  });
 
   const render = () => {
 
@@ -108,11 +67,11 @@ window.addEventListener('load', () => {
       changeSizeObserver.width,
       changeSizeObserver.height
     );
-    origin.set(TILE_SIZE.x * 5 + drag.x, TILE_SIZE.y * 5 + drag.y);
+    origin.set(TILE_SIZE.x * 5 + panningListener.drag.x, TILE_SIZE.y * 5 + panningListener.drag.y);
 
     const vMouse = {
-      x: cursor.x - (5 * TILE_SIZE.x + drag.x),
-      y: cursor.y - (5 * TILE_SIZE.y + drag.y)
+      x: cursor.x - (5 * TILE_SIZE.x + panningListener.drag.x),
+      y: cursor.y - (5 * TILE_SIZE.y + panningListener.drag.y)
     } as Vec2D;
     const vSelected = {
       x: Math.floor((vMouse.x + 2 * vMouse.y - Math.floor( TILE_SIZE.x / 2 )) / TILE_SIZE.x),
---
+++ src/ui/infra/CanvasPanningListener.ts
@@ -0,0 +1,57 @@
+import { Vec2D } from "@/common/Vec2D";
+
+export default class CanvasPanningListener {
+  readonly _canvas: HTMLCanvasElement;
+  private _dragging: boolean;
+  private _drag: Vec2D;
+  private _dragstart: Vec2D;
+  private _currentDragging: Vec2D;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this._canvas = canvas;
+    this._dragging = false;
+    this._drag = new Vec2D();
+    this._dragstart = new Vec2D();
+    this._currentDragging = new Vec2D();
+
+    this._canvas.addEventListener('mousedown', (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      this._dragging = true;
+      this._currentDragging = new Vec2D(this._drag);
+      this._dragstart.set(
+        e.clientX * window.devicePixelRatio,
+        e.clientY * window.devicePixelRatio
+      )
+    });
+
+    canvas.addEventListener('mouseup', (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopPropagation();
+      this._dragging = false;
+    });
+
+    canvas.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!this._dragging) {
+        return;
+      }
+      const currentPosition = new Vec2D(
+        e.clientX * window.devicePixelRatio,
+        e.clientY * window.devicePixelRatio
+      )
+      if (this._dragstart.almostEq(currentPosition, 2)) {
+        return;
+      }
+
+      this._drag.set(new Vec2D(
+        e.clientX * window.devicePixelRatio - this._dragstart.x + this._currentDragging.x,
+        e.clientY * window.devicePixelRatio - this._dragstart.y + this._currentDragging.y
+      ));
+    });
+  }
+
+  public get drag(): Vec2D {
+    return this._drag;
+  };
+
+}
