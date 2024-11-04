2bb61cf182c518bc80fafac76c04a02760b3366a
SamuelLorrain <samuel.lorrain@gmail.com>
2024-11-04 20:01:03 +0100

    common: create Vec3D

---
+++ src/common/Vec3D.ts
@@ -0,0 +1,58 @@
+export class Vec3D {
+  public x: number;
+  public y: number;
+  public z: number;
+
+  constructor();
+  constructor(x: number, y: number, z: number);
+  constructor(x: number | Vec3D, y?: never, z?: never);
+  constructor(x: number | Vec3D = 0, y: number = 0, z: number = 0) {
+    if (x instanceof Vec3D) {
+      this.x = x.x;
+      this.y = x.y;
+      this.z = x.z;
+    } else {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+  }
+
+  public set(x: number, y: number, z: number): void;
+  public set(v: Vec3D, y?: never, z?: never): void;
+  public set(x: number | Vec3D, y: number = 0, z:number = 0) {
+    if (x instanceof Vec3D) {
+      this.x = x.x;
+      this.y = x.y;
+      this.z = x.z;
+    } else {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+  }
+
+  public add(v: Vec3D) {
+    return new Vec3D(
+      this.x + v.x,
+      this.y + v.y,
+      this.z + v.z
+    )
+  }
+
+  public sub(v: Vec3D) {
+    return new Vec3D(
+      this.x - v.x,
+      this.y - v.y,
+      this.z - v.z
+    )
+  }
+
+  public eq(v: Vec3D): boolean {
+    return this.x === v.x && this.y === v.y && this.z == v.z;
+  }
+
+  public almostEq(v: Vec3D, delta: number): boolean {
+    return Math.abs(this.x - v.x) <= delta && Math.abs(this.y - v.y) <= delta && Math.abs(this.z - v.z) <= delta;
+  }
+}
