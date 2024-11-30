dc2c966b77979300d44da5cf025ca14d418b9630
SamuelLorrain <samuel.lorrain@gmail.com>
2024-11-30 17:43:40 +0100

    game(refactor): create Selector abstraction

--- src/game/Selector.ts
+++ src/game/Selector.ts
@@ -1,59 +1,80 @@
 import { Vec2D } from "@/common/Vec2D";
-import Mouse from "@/ui/infra/Mouse";
-import { WorldMap } from "./WorldMap";
-import { Tile } from "./Tile";
-import Context2DProvider from "@/ui/infra/Context2DProvider";
-import { Publisher } from "@/common/behavioral/Publisher";
-import { Observer } from "@/common/behavioral/Observer";
+import Mouse from "@/ui/Mouse";
+import Context2DProvider from "@/ui/Context2DProvider";
+import { DrawnTile, Renderer } from "./Renderer";
+import { Vec3D } from "@/common/Vec3D";
+import { Queue } from "@/common/Queue";
 
-export class Selector implements Publisher {
+export interface ClickEvent {
+  tile?: Vec3D;
+  pixel?: Vec2D;
+  kind: string;
+};
+
+export interface ClickTileEvent extends ClickEvent {
+  tile: Vec3D;
+  pixel: Vec2D;
+  kind: "ClickTileEvent";
+};
+
+export interface ClickPixelEvent extends ClickEvent {
+  pixel: Vec2D;
+  kind: "ClickPixelEvent";
+};
+
+export const isClickTileEvent = (clickEvent: ClickEvent): clickEvent is ClickTileEvent => {
+  return clickEvent.tile instanceof Vec3D;
+}
+
+export const isClickPixelEvent = (clickEvent: ClickEvent): clickEvent is ClickPixelEvent => {
+  return clickEvent.tile == null;
+}
+
+export class Selector {
   private _mouse: Mouse;
-  private _hoverTile?: Tile;
+  private _hoverTile?: DrawnTile;
   private _cursorPositionOnMouseDown?: Vec2D;
-  private _context2dProvider: Context2DProvider;
-  private _observers: Observer[];
+  private _context2DProvider: Context2DProvider;
+  private _renderer: Renderer;
+  private _clickEventQueue: Queue<ClickEvent>;
 
-  constructor (
-    mouse: Mouse,
-    context2DProvider: Context2DProvider,
-    onClick: (e: MouseEvent) => void
-  ) {
-    this._mouse = mouse;
+  constructor (renderer: Renderer) {
+    this._mouse = Mouse.getInstance();
+    this._context2DProvider = Context2DProvider.getInstance();
     this._hoverTile = undefined;
     this._cursorPositionOnMouseDown = undefined;
-    this._context2dProvider = context2DProvider;
+    this._renderer = renderer;
+    this._clickEventQueue = new Queue();
     this._initMouseDown();
-    this._initMouseUp(onClick);
-    this._observers = [];
+    this._initMouseUp();
   }
 
   private _initMouseDown() {
-    this._context2dProvider.canvas.addEventListener('mousedown', (e: MouseEvent) => {
+    this._context2DProvider.canvas.addEventListener('mousedown', (e: MouseEvent) => {
       this._cursorPositionOnMouseDown = new Vec2D(e.clientX, e.clientY);
     });
   }
 
-  public addObserver(observer: Observer) {
-    this._observers.push(observer);
-  }
-
-  public notify() {
-    for(const observer of this._observers) {
-      observer.update({
-        data: null,
-        eventType: "ClickEvent"
-      })
-    }
-  }
-
-  private _initMouseUp(f: (e: MouseEvent) => void) {
-    this._context2dProvider.canvas.addEventListener('mousedown', (e: MouseEvent) => {
+  private _initMouseUp() {
+    this._context2DProvider.canvas.addEventListener('mouseup', (e: MouseEvent) => {
       const cursorPositionOnMouseUp = new Vec2D(e.clientX, e.clientY);
       if (this._cursorPositionOnMouseDown == null || !this._cursorPositionOnMouseDown.almostEq(cursorPositionOnMouseUp, 3)) {
         return;
       }
-      f(e);
-      this.notify();
+      if (this._hoverTile != null) {
+        const event = {
+          tile: this._hoverTile.tile.pos,
+          pixel: cursorPositionOnMouseUp,
+          kind: "ClickTileEvent"
+        } satisfies ClickTileEvent;
+        this._clickEventQueue.enqueue(event);
+      } else {
+        const event = {
+          pixel: cursorPositionOnMouseUp,
+          kind: "ClickPixelEvent"
+        } satisfies ClickPixelEvent;
+        this._clickEventQueue.enqueue(event);
+      }
     });
   }
 
@@ -61,61 +82,66 @@ export class Selector implements Publisher {
     return this._hoverTile;
   }
 
-  updateHoverTile(origin: Vec2D, map: WorldMap) {
-    const cursor = this._mouse.vec;
-    const firstPassHoverTiles: Tile[] = [];
-    for (let tileTower of map.tiles) {
-      for (let tile of tileTower) {
-        if (tile.spriteNb < 0) {
-          continue;
-        }
-        const drawPos = tile.drawPos.add(origin);
+  public hasPendingEvent() {
+    return !this._clickEventQueue.empty();
+  }
 
-        /*
-        * Optimisation.
+  public getEvent() {
+    return this._clickEventQueue.dequeue();
+  }
+
+  public updateHoverTile() {
+    let mousePos = this._mouse.pos;
+    const firstPassHoverTiles = [];
+    if (this._renderer.drawnTiles.length === 0) {
+      return;
+    }
+    for (let dTile of this._renderer.drawnTiles) {
+
+      const spriteSheet = dTile.tile.sprite.spriteSheet;
+      const realDrawPos = dTile.drawPos.add(this._context2DProvider.origin);
+
+      /* Optimisation.
         * Only verify pixels on "almost certains" tiles.
         * TODO verify if the optimisation worth.
         */
-        if ((drawPos.x < cursor.x && (drawPos.x + tile.spriteSheet.size.x) >= cursor.x)
-            &&
-            (drawPos.y < cursor.y && (drawPos.y + tile.spriteSheet.size.y) >= cursor.y)) {
-          firstPassHoverTiles.push(tile);
-        }
+      if ((realDrawPos.x < mousePos.x && (realDrawPos.x + spriteSheet.sizeOfSprite.x) >= mousePos.x)
+        &&
+        (realDrawPos.y < mousePos.y && (realDrawPos.y + spriteSheet.sizeOfSprite.y) >= mousePos.y)) {
+        firstPassHoverTiles.push(dTile);
       }
     }
+    const secondPassHoverTiles: DrawnTile[] = [];
 
-    const secondPassHoverTiles: Tile[] = [];
-    const mousePos = cursor.sub(origin);
+    mousePos = this._mouse.pos.sub(this._context2DProvider.origin);
     mousePos.set(
       Math.round(mousePos.x),
       Math.round(mousePos.y),
     )
-    for (let tile of firstPassHoverTiles.reverse()) {
-      const spritePosition = tile.spriteSheet.getSprite(tile.spriteNb);
-      const spriteSheetImageData = tile.spriteSheet.picture.imageData;
 
-      const y = spritePosition.position.y;
-      const x = spritePosition.position.x;
+    for (let dTile of firstPassHoverTiles.reverse()) {
+      const spriteSheet = dTile.tile.sprite.spriteSheet;
+      const spritePosition = spriteSheet.getSpritePosition(dTile.tile.sprite.spriteNb);
+      const spriteSheetImageData = spriteSheet.picture.imageData;
+
+      const y = spritePosition.y;
+      const x = spritePosition.x;
       const width = spriteSheetImageData.width;
 
-      const drawPos = tile.drawPos;
+      const drawPos = dTile.drawPos;
       const hoverPixelPosOnATile = mousePos.sub(drawPos);
-      if ((hoverPixelPosOnATile.x >= spritePosition.size.x)
-        || (hoverPixelPosOnATile.y >= spritePosition.size.y)) {
+      if ((hoverPixelPosOnATile.x >= spriteSheet.sizeOfSprite.x)
+        || (hoverPixelPosOnATile.y >= spriteSheet.sizeOfSprite.y)) {
         continue;
       }
       const pixelRGBAPosition = (width*(y+hoverPixelPosOnATile.y)+(x+hoverPixelPosOnATile.x))*4;
 
       if (spriteSheetImageData.data[pixelRGBAPosition+3] > 0) {
-        secondPassHoverTiles.push(tile);
+        secondPassHoverTiles.push(dTile);
         break;
       }
     }
-    const hoverTile = secondPassHoverTiles[0];
-    if (hoverTile?.blocked) {
-      return this._hoverTile = undefined;
-    }
+
     this._hoverTile = secondPassHoverTiles[0];
   }
-
 }
