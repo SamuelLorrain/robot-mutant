a4a9a7e6fdca77f3a745b7be06b51fbac83a58ec
SamuelLorrain <samuel.lorrain@gmail.com>
2024-12-15 23:58:21 +0100

    feat(tilesToGraph): allow to project 3D tiles to 2DGrid

--- src/game/TilesToGraph.ts
+++ src/game/TilesToGraph.ts
@@ -2,8 +2,16 @@ import { Graph } from "@/common/Graph";
 import { Vec2D } from "@/common/Vec2D";
 import { Hash } from "@/common/Hash";
 import { Tile } from "./Tile";
+import { Vec3D } from "@/common/Vec3D";
 
-export const tiles2DToGraph = (tiles: Map<Hash, Tile>): Graph => {
+export const tiles3DToGraph2D = (tiles: Map<Hash, Tile>): Graph => {
+  const tiles2D: Map<Hash, Tile> = new Map();
+
+  for (const [k,v] of tiles.entries()) {
+    const vec = Vec3D.unhash(k);
+    tiles2D.set((new Vec2D(vec.x, vec.y)).hash(), v);
+  }
+
   const map = new Map<Hash, Hash[]>();
 
   for (const [k, _] of tiles) {
@@ -18,7 +26,7 @@ export const tiles2DToGraph = (tiles: Map<Hash, Tile>): Graph => {
     ];
 
     for(const vec of n) {
-      if (tiles.get(vec.hash())) {
+      if (tiles2D.get(vec.hash())) {
         neighbours.push(vec.hash());
       }
     }
